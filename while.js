28baad161e83ff2b092042521fb8453081fd1161
ElisFliess <102757856+Elisfliess@users.noreply.github.com>
2022-05-16 14:20:24 -0300

    Add files via upload

---
+++ while.js
@@ -0,0 +1,33 @@
+//01 1. Faça um programa que exiba os números de 1 a 100.
+
+
+
+let c = 1
+while (c <= 100){
+    console.log(c)
+    c = c + 1
+} 
+
+//02 2. Faça um programa que exiba os números de 50 a 100.
+
+let c = 50
+while (c <= 100){
+    console.log(c)
+    c = c + 1
+}
+
+//03 Faça um programa que escreva a contagem regressiva do lançamento de um foguete. O programa deve imprimir 10,9, 8, ...,1,0.
+
+let c = 10
+while (c >= 0){
+    console.log(c)
+    c = c - 1
+}
+
+//04 4. Faça um programa que exiba os pares de 0 a 20.
+
+let c = 0
+while (c <= 20){
+    console.log(c)
+    c = c + 2
+}
